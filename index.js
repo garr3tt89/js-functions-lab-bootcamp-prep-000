6820fbfed6a9ac9a5411633f2989da0bc62623ed
garr3tt1989@gmail.com Hausman <garr3tt1989@gmail.com>
2018-08-13 18:27:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// write your code below!
+describe('functions', () => {
+  describe('happyHolidays', () => {
+    it('returns "Happy holidays!"', () => {
+      expect(happyHolidays()).toEqual('Happy holidays!')
+    })
+  })
+
+  describe('happyHolidaysTo(name)', () => {
+    it('returns "Happy holidays, ${name}!"', () => {
+      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
+    })
+  })
+
+  describe('happyHolidayTo(holiday, name)', () => {
+    it('returns "Happy ${holiday}, ${name}!"', () => {
+      expect(happyHolidayTo('Fathers Day', 'you')).toEqual('Happy Fathers Day, you!')
+    })
+  })
+
+  describe('holidayCountdown(holiday, days)', () => {
+    it('returns "It\'s ${days} days until ${holiday}!"', () => {
+      expect(holidayCountdown("Christmas", 20)).toEqual("It's 4 days until Christmas!")
+    })
+  })
+})
