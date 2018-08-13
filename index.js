b11544d78a3184935946a7366c3f8a01419bbf1f
garr3tt1989@gmail.com Hausman <garr3tt1989@gmail.com>
2018-08-13 18:29:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 /*global describe, it*/
 
 describe('functions', () => {
-  describe('happyHolidays', () => {
-    it('returns "Happy holidays!"', () => {
+  describe('happyHolidays', () => 
+    it('returns "Happy holidays!"', () => 
       expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
+  }
 
   describe('happyHolidaysTo(name)', () => {
     it('returns "Happy holidays, ${name}!"', () => {
@@ -22,6 +21,6 @@ describe('functions', () => {
   describe('holidayCountdown(holiday, days)', () => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
       expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-    })
-  })
-})
+    }
+  }
+}
